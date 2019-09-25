943ba79aa61c88708ba9431aa999e246a4a556b1
Luis <luiscarlos.sf@outlook.com>
2019-09-25 01:44:55 -0300

    Adiciona classes das redes

--- rna.py
+++ rna.py
@@ -1,27 +1,32 @@
-import random
+from random import uniform
+import math 
+import numpy as np
 
-class Neuron():
-    def __init__(self, inputs, activation, beta=None, weights=None, *args, **kwargs):
-        self.inputs = [-1].extend(inputs)
+class Neuron:
+    def __init__(self,  inputs_size, activation=None, beta=1):
         self.activation = activation
         self.beta = beta
-        if weights == None:
-            self.weigths = [random.rand(0,1) for i in range(len(self.inputs))]
-        else:
-            self.weights = weights
-        
-        super().__init__(*args, **kwargs)
+        self.weights = np.array([uniform(0,1) for i in range(np.size(inputs_size + 1))])
     
-    def get_output():
-        pass
+    def get_output(self, inputs):
+        
+        _inputs = [-1]
+        _inputs.extend(inputs)
+        
+        _inputs = np.array(_inputs)
+        if np.size(_inputs) != np.size(self.weights):
+            raise ValueError("Quantidade de entradas menor do que a de pesos")
+        u = (_inputs * self.weights).sum()
+        y = self.activation(u, self.beta)
+        return y
 
-def degree_bipolar(number, beta):
+def degree_bipolar(number, beta=None):
     if number >= 0:
         return 1
     else:
         return -1
 
-def degree(number, beta):
+def degree(number, beta=None):
     if number >= 0:
         return 1
     else:
@@ -30,17 +35,61 @@ def degree(number, beta):
 def tan_hiperbolic(number, beta):
     return (1-math.e**((-beta)*number))/(1+math.e**((-beta)*number))
 
-def logarithmic(number, beta):
+def logistic(number, beta):
     return 1/(1+math.e**((-beta)*number))
+
+def linear(number, beta):
+    return number
 
 
 class HiddenLayer:
-    def __init__(self, size, activation, *args, **kwargs):
+    def __init__(self, inputs_size, size, activation=None, beta=1, *args, **kwargs):
+        self.size = size
+        self.neurons = [ Neuron(inputs_size, activation=activation, beta=beta) for i in range(size)]
         super().__init__(*args, **kwargs)
+    
+    def set_activation(self, activation, neuron_index=None):
+        if neuron_index == None:
+            for i in range(self.size):
+                self.neurons[i].activation = activation
+        else:
+            self.neurons[neuron_index - 1].activation = activation
+    
+    def set_weights(self, weigths, neuron_index):
+        self.neurons[neuron_index - 1].weights = weigths
+    
+    def set_threshold(self, threshold, neuron_index):
+        self.neurons[neuron_index - 1].weights[0] = threshold
+    
+    def set_beta(self, beta, neuron_index):
+        self.neurons[neuron_index - 1].beta = beta
+
+    def get_output(self, inputs):
+        return [neuron.get_output(inputs) for neuron in self.neurons]
 
 class OutputLayer:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,inputs_size, size, activation=None, beta=1):
+        self.size = size
+        self.neurons = [ Neuron(inputs_size, activation=activation, beta=beta) for i in range(size)]
+    
+    def set_activation(self, activation, neuron_index=None):
+        if neuron_index == None:
+            for i in range(self.size):
+                self.neurons[i].activation = activation
+        else:
+            self.neurons[neuron_index - 1].activation = activation
+
+    def set_weights(self, weights, neuron_index):
+        self.neurons[neuron_index - 1].weights = weights
+    
+    def set_threshold(self, threshold, neuron_index):
+        self.neurons[neuron_index - 1].weights[0] = threshold
+    
+    def set_beta(self, beta, neuron_index):
+        self.neurons[neuron_index - 1].beta = beta
+    
+    def get_output(self, inputs):
+        return [i.get_output(inputs) for i in self.neurons]
 
 class InputLayer:
     def __init__(self, inputs, *args, **kwargs):
@@ -48,5 +97,8 @@ class InputLayer:
         self.size = len(self.inputs)
         super().__init__(*args, **kwargs)
     
-    def getSize():
+    def get_size(self):
         return self.size
+    
+    def get_inputs(self):
+        return self.inputs
