a211187f240dc6d4d5bbbb66591267a04b4c6541
Luis <luiscarlos.sf@outlook.com>
2019-09-26 01:36:30 -0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,9 +1,10 @@
 import rna
-from feedforward import MultiFeedforward
+from feedforward import Feedforward
 from xlrd import open_workbook
+import numpy as np
 
 def iib():
-    network = MultiFeedforward(inputs_size=1, hidden_sizes=[3], outputs_size=1)
+    network = Feedforward(inputs_size=1, hidden_sizes=[3], outputs_size=1)
 
     network.set_weigths(weights=[3.38888, 0.962803], layer_index=1, neuron_index=1)
     network.set_weigths(weights=[11.0847, 1.88752], layer_index=1, neuron_index=2)
@@ -17,3 +18,18 @@ def iib():
     xlsx_file = open_workbook('RNA_Aproximação_Universal_PPB.xls')
     values_x1 = [i.value for i in xlsx_file.sheet_by_index(0).col(0)[1:]]
     print([network.get_outputs([i]) for i in values_x1])
+
+def iia():
+    wt= [[0,0,0],[0.5, 1, 1], [0.25, 0.5, 1], [-0.5,-0.5,1], [-1,-1,-1]] #Settings of weights and thresholds
+    N = 20 #número
+    inputs = [[i, i] for i in np.linspace(0, 1, N, endpoint=True)]
+    network = Feedforward(inputs_size=2, outputs_size=1, activation=rna.logistic, beta=1)
+
+def l3p2a():
+    pass
+
+def l3p2b():
+    pass
+
+def l5p2():
+    pass
